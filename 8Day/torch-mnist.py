75756318fd471c42bb22a079fb3bf4bb04872a86
hwc9169 <hwc9169@gmail.com>
2020-07-10 15:26:09 +0900

    update

--- 8Day/torch-mnist.py
+++ 8Day/torch-mnist.py
@@ -37,6 +37,7 @@ data_loader = DataLoader(dataset = train,
 linear = nn.Linear(784, 10, bias=True).to(device)
 criterion = nn.CrossEntropyLoss().to(device)
 optimizer = torch.optim.SGD(linear.parameters(),lr=0.1)
+
 for epoch in range(epochs+1): # 앞서 training_epochs의 값은 15로 지정함.
     avg_cost = 0
     total_batch = len(data_loader)
@@ -46,10 +47,10 @@ for epoch in range(epochs+1): # 앞서 training_epochs의 값은 15로 지정함
         X = X.view(-1, 28 * 28).to(device)
         # 레이블은 원-핫 인코딩이 된 상태가 아니라 0 ~ 9의 정수.
         Y = Y.to(device)
-
-        optimizer.zero_grad()
         hypothesis = linear(X)
         cost = criterion(hypothesis, Y)
+
+        optimizer.zero_grad()
         cost.backward()
         optimizer.step()
 
@@ -58,4 +59,22 @@ for epoch in range(epochs+1): # 앞서 training_epochs의 값은 15로 지정함
     print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
 print('Learning finished')
 
+with torch.no_grad():
+    x_test = test.test_data.view(-1,28,28).float().to(device)
+    y_test = test.test_labels.to(device)
 
+    prediction = linear(x_test)
+    correct_prediction = torch.argmax(prediction,1) ==y_test
+    accuracy = correct_prediction.float().mean()
+    print('Accuracy:',accuracy.item())
+
+    r = random.randint(0, len(test) - 1)
+    x_single_data = test.test_labels[r:r+1].view(-1,28*28).float().to(device)
+    y_single_data = test.test_labels[r:r+1].to(device)
+
+    print('Label: ',y_single_data.item())
+    single_prediction = linear(x_single_data)
+    print('Prediction', torch.argmax(single_prediction,1).item())
+
+    plt.imshow(test.test_data[r:r+1].view(28,28), cmap='Greys' , interpolation='nearest')
+    plt.show()
